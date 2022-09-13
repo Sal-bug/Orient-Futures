fdf1853e879044f8852310a322094e8f3d0ac3aa
Sal-bug <66895728+Sal-bug@users.noreply.github.com>
2022-09-13 17:09:29 -0400

    Modify Login codes

--- v6.6.8_T1_20220520_api_tradeapi_se_linux64/main.cpp
+++ v6.6.8_T1_20220520_api_tradeapi_se_linux64/main.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
-#include "ThostFtdcTraderApi.h"
 #include <cstring>
+
+#include "ThostFtdcTraderApi.h"
+#include "ThostFtdcMdApi.h"
+#include "ThostFtdcUserApiDataType.h"
+#include "ThostFtdcUserApiStruct.h"
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
 using namespace std;
 
+sem_t sem;
+
+class CTraderHandler : public CThostFtdcTraderSpi{
+
+	public:
+
+    CTraderHandler(){
+        printf("CTraderHandler:called.\n");
+    }
+
+    virtual void OnFrontConnected() {
+		static int i = 0;
+		// 在登出后系统会重新调用OnFrontConnected，这里简单判断并忽略第1次之后的所有调用。
+		if (i++ == 0) {
+        	printf("OnFrontConnected:called.\n");
+			sem_post(&sem);
+		}
+    }
+
+    virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,
+        CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+        printf("OnRspUserLogin:called\n");
+		if (pRspInfo->ErrorID == 0) {
+			printf("Login Success!\n");			
+			sem_post(&sem);
+		}
+        else{
+			printf("Login Failure!\n");
+		}
+    }
+
+	// virtual void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, 
+	// 		CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {	
+	// 	printf("OnReqUserLogout:called\n");
+	// 	if (pRspInfo->ErrorID == 0) {
+	// 		printf("登出成功!\n");
+	// 		sem_post(&sem);
+	// 	}else{
+	// 		printf("登出失败!\n");
+	// 	}
+	// }
+};
+
 int main(int argc, char* argv[]) {
-    char frontAddress[] = "tcp://180.168.146.187:10201";
-    char BROKERID[] = "9999";
-    char USERID[] = "9999";
-    char PASSWORD[] = "9999";
+    // 初始化变量
+	sem_init(&sem,0,0);
+    char * serverAddress = (char *)"tcp://180.168.146.187:10201";
+    CThostFtdcReqUserLoginField userLoginField;
 
-
-    CThostFtdcMdApi* pUserApi = CThostFtdcMdApi::CreateFtdcMdApi("./", false, false);
-    CThostFtdcMdSpi* pUserSpi = new CThostFtdcMdSpi();
-    pUserApi->RegisterSpi(pUserSpi);
-    pUserApi->RegisterFront(frontAddress);
-    pUserApi->Init();
-    pUserApi->Join();
-
-    CThostFtdcRspUserLoginField req;
-    memset(&req, 0, sizeof(req));
-    strcpy(req.BrokerID, BROKERID);
-    strcpy(req.UserID, USERID);
-    strcpy(req.Password, PASSWORD);
-
-
-
-     // 创建TraderAPI和回调响应控制器的实例
+    // 创建TraderAPI和回调响应控制器的实例
     CThostFtdcTraderApi *pTraderApi = CThostFtdcTraderApi::CreateFtdcTraderApi();
     CTraderHandler traderHandler = CTraderHandler();
     CTraderHandler * pTraderHandler = &traderHandler;
@@ -40,14 +76,9 @@ int main(int argc, char* argv[]) {
 	sem_wait(&sem);
 	
 	// 读取登录信息
-	printf("BrokerID:");
-    scanf("%s", (char*) &userLoginField.BrokerID);
-    
-    printf("userid:");
-    scanf("%s", (char*) &userLoginField.UserID);
-        
-    printf("password:");
-    scanf("%s", (char*) &userLoginField.Password);
+	strcpy(userLoginField.BrokerID, "9999");
+    strcpy(userLoginField.UserID, "206045");
+    scanf(userLoginField.Password, "Thzss200718");
     
 	// 发出登陆请求
 	pTraderApi->ReqUserLogin(&userLoginField, 0);
@@ -59,15 +90,15 @@ int main(int argc, char* argv[]) {
 	sleep(5);
 	printf("等待5秒完毕\n");
 
-	// 拷贝用户登录信息到登出信息
-	strcpy(userLogoutField.BrokerID,userLoginField.BrokerID);
-	strcpy(userLogoutField.UserID, userLoginField.UserID);
-	pTraderApi->ReqUserLogout(&userLogoutField, 1);
+	// // 拷贝用户登录信息到登出信息
+	// strcpy(userLogoutField.BrokerID,userLoginField.BrokerID);
+	// strcpy(userLogoutField.UserID, userLoginField.UserID);
+	// pTraderApi->ReqUserLogout(&userLogoutField, 1);
 
-	// 等待登出成功
-	sem_wait(&sem);
-		
+	// // 等待登出成功
+	// sem_wait(&sem);
 	
 	printf("主线程执行完毕!\n");
+    return(0);
 
 }
